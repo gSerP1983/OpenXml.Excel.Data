b5ebd1409f8a25a9d887280b9a943016a53ac540
gSerP1983 <gserp1983@gmail.com>
2016-01-31 20:38:04 +0300

    Add tests

--- UnitTest.OpenXml.Excel.Data/ExcelDataReaderTest.cs
+++ UnitTest.OpenXml.Excel.Data/ExcelDataReaderTest.cs
@@ -10,7 +10,7 @@ namespace UnitTest.OpenXml.Excel.Data
     public class ExcelDataReaderTest
     {
         [TestMethod]
-        public void OpenPathHdrDefaultSheet()
+        public void OpenPathHdrDefaultSheetTest()
         {
             var dt = new DataTable();
             using (var reader = new ExcelDataReader(@"test.xlsx"))
@@ -21,7 +21,7 @@ namespace UnitTest.OpenXml.Excel.Data
         }
 
         [TestMethod]
-        public void OpenPathNoHdrSheetName()
+        public void OpenPathNoHdrSheetNameTest()
         {
             var dt = new DataTable();
             using (var reader = new ExcelDataReader(@"test.xlsx", "Second", false))
@@ -32,7 +32,7 @@ namespace UnitTest.OpenXml.Excel.Data
         }
 
         [TestMethod]
-        public void OpenStreamHdrSheetIndex()
+        public void OpenStreamHdrSheetIndexTest()
         {
             var dt = new DataTable();
 
@@ -47,7 +47,7 @@ namespace UnitTest.OpenXml.Excel.Data
         }
 
         [TestMethod]
-        public void OpenStreamEmptySheet()
+        public void OpenStreamEmptySheetTest()
         {
             var dt = new DataTable();
 
@@ -63,7 +63,7 @@ namespace UnitTest.OpenXml.Excel.Data
 
         [TestMethod]
         [ExpectedException(typeof(ApplicationException))]
-        public void OpenInvalidSheetIndex()
+        public void OpenInvalidSheetIndexTest()
         {
             using (new ExcelDataReader(@"test.xlsx", 27))
             {
@@ -72,10 +72,47 @@ namespace UnitTest.OpenXml.Excel.Data
 
         [TestMethod]
         [ExpectedException(typeof(ApplicationException))]
-        public void OpenInvalidSheetName()
+        public void OpenInvalidSheetNameTest()
         {
             using (new ExcelDataReader(@"test.xlsx", "Yuri Gagarin"))
             {
+            }
+        }
+
+        [TestMethod]
+        public void DataReaderTest()
+        {
+            using (var reader = new ExcelDataReader(@"test.xlsx"))
+            {
+                Assert.AreEqual(0, reader.Depth);
+                Assert.AreEqual(-1, reader.RecordsAffected);
+                Assert.AreEqual(7, reader.FieldCount);
+                Assert.AreEqual(5, reader.GetOrdinal("ColDecimal"));
+                Assert.AreEqual(-1, reader.GetOrdinal("InvalidColName"));
+            }
+        }
+
+        [TestMethod]
+        public void ReadTest()
+        {
+            using (var reader = new ExcelDataReader(@"test.xlsx"))
+            {
+                reader.Read();
+                Assert.AreEqual(381728, reader.GetInt32(0));
+                Assert.AreEqual(381728, reader.GetInt64(0));
+
+                Assert.AreEqual("Mr Brown", reader.GetString(1));
+                Assert.AreEqual("Mr Brown", reader[1]);
+                Assert.AreEqual("Mr Brown", reader["Name"]);
+
+                Assert.AreEqual(new Guid("6E2BF784-F116-494A-916D-9DFF9B2A2AA0"), reader.GetGuid(3));
+
+                Assert.AreEqual(32, reader.GetInt16(4));
+                Assert.AreEqual(32, reader.GetInt32(4));
+                Assert.AreEqual(32, reader.GetInt64(4));
+                Assert.AreEqual(32, reader.GetByte(4));
+
+                Assert.AreEqual(true, reader.GetBoolean(6));
             }
         }
     }
