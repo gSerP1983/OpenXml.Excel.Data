3478255bb64bf7fc3e2ac6b32f07b5f16539234b
gSerP1983 <gserp1983@gmail.com>
2016-02-26 20:34:42 +0300

    New samples

---
+++ OpenXml.Excel.Data.Test/DataHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace OpenXml.Excel.Data.Test
+{
+    class DataHelper
+    {
+        private const int MaxLength = 8000;
+
+        public static void CreateTableIfNotExists(string connection, string tableName, DataTable dt)
+        {
+            const string pattern = @"IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID('{0}') AND [type] in ('U'))
+BEGIN
+{1}
+END";
+
+            var createScript = string.Format(pattern, tableName, GetCreateTableScript(tableName, dt));
+            ExecuteNonQuery(connection, createScript);
+        }
+
+        private static void ExecuteNonQuery(string connection, string sql)
+        {
+            using (var conn = new SqlConnection(connection))
+            {
+                conn.Open();
+                using (var cmd = new SqlCommand(sql, conn) { CommandTimeout = 0 })
+                    cmd.ExecuteNonQuery();
+            }
+        }
+
+        private static string GetCreateTableScript(string tableName, DataTable dt)
+        {
+            var sql = new StringBuilder("CREATE TABLE " + tableName);
+            sql.AppendLine("(");
+            sql.AppendLine("\tLINK int IDENTITY(1,1) NOT NULL,");
+            sql.AppendLine("\tSESSION_ID uniqueidentifier NULL,");
+
+            foreach (DataColumn col in dt.Columns)
+                sql.AppendLine("\t[" + col.ColumnName + "] varchar(" + MaxLength + ") NULL,");
+
+            sql.AppendLine("CONSTRAINT PK_" + AdjustTableName(tableName) + " PRIMARY KEY CLUSTERED");
+            sql.AppendLine("(LINK ASC)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON)");
+            sql.AppendLine(")");
+
+            return sql.ToString();
+        }
+
+        private static string AdjustTableName(string tableName)
+        {
+            return tableName.Replace(".", "_");
+        }
+    }
+}
--- OpenXml.Excel.Data.Test/Program.cs
+++ OpenXml.Excel.Data.Test/Program.cs
@@ -1,18 +1,42 @@
 ﻿using System;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace OpenXml.Excel.Data.Test
 {
     class Program
     {
+        private const string ConnectionString = @"Server=(local)\sqlexpress;Database=TestDb;Trusted_Connection=True;";
+        private const string TableName = "ImportTable";
+
         static void Main()
         {
-            var dt = new DataTable();
-            using (var reader = new ExcelDataReader(@"test.xlsx"))           
-                dt.Load(reader);
-
-            Console.WriteLine("done: " + dt.Rows.Count);
+            DataTableSample();
             Console.ReadKey();
+        }
+
+        private static void DataTableSample()
+        {
+            // read to Datatable sample
+            var dt = new DataTable();
+            using (var reader = new ExcelDataReader(@"test.xlsx"))
+                dt.Load(reader);
+            Console.WriteLine("Read DataTable done: " + dt.Rows.Count);
+
+            // create table in database by DataTable
+            DataHelper.CreateTableIfNotExists(ConnectionString, TableName, dt);
+            Console.WriteLine("Create database table done.");
+
+            // very fast way to copy data to sql server
+            using (var bulkCopy = new SqlBulkCopy(ConnectionString))
+            {
+                bulkCopy.DestinationTableName = TableName;
+                foreach (DataColumn dc in dt.Columns)
+                    bulkCopy.ColumnMappings.Add(dc.ColumnName, dc.ColumnName);
+
+                bulkCopy.WriteToServer(dt);
+            }
+            Console.WriteLine("Copy data ti database done.");
         }
     }
 }
